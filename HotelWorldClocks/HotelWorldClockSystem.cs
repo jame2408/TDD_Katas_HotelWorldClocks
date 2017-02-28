cce386886a66209613e4ce993898fb55c54d686b
jame2408 <jame2408@gmail.com>
2017-02-28 16:20:31 +0800

    Created method HotelWorldClockSystem.getClocks() to return a clock list stored in this class.

--- HotelWorldClocks/HotelWorldClockSystem.cs
+++ HotelWorldClocks/HotelWorldClockSystem.cs
@@ -1,10 +1,17 @@
-﻿namespace HotelWorldClocks
+﻿using System.Collections.Generic;
+
+namespace HotelWorldClocks
 {
     public class HotelWorldClockSystem
     {
         public void attach(CityClock cityClock)
         {
 
+        }
+
+        public IEnumerable<CityClock> getClocks()
+        {
+            return this.cityClocks;
         }
     }
 }
