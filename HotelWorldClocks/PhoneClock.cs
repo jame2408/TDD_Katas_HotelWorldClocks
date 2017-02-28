32b1babd5c7a8d6dc6d709919e312e543cee21d5
jame2408 <jame2408@gmail.com>
2017-02-28 17:47:42 +0800

    Made class PhoneClock use the field uctOffset from the super class Clock.

--- HotelWorldClocks/PhoneClock.cs
+++ HotelWorldClocks/PhoneClock.cs
@@ -1,21 +1,20 @@
 ﻿namespace HotelWorldClocks
 {
-    public class PhoneClock
+    public class PhoneClock : Clock
     {
-        private int _utcOffset;
         private HotelWorldClockSystem _hotelWorldClockSystem;
 
         public PhoneClock(int utcOffset)
         {
-            this._utcOffset = utcOffset;
+            _utcOffset = utcOffset;
         }
-        
+
 
         public void setTime(int time)
         {
             foreach (CityClock cityClock in this._hotelWorldClockSystem.getClocks())
             {
-                cityClock.setUtcZeroTime(time - this._utcOffset);
+                cityClock.setUtcZeroTime(time - _utcOffset);
             }
         }
 
