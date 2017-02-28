ae52a990dbd3c1056d6e3e335c2f346514e5bba7
jame2408 <jame2408@gmail.com>
2017-02-28 15:54:51 +0800

    Wrote intention code in test the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time().

--- HotelWorldClocksTest/HotelWorldClocksTest.cs
+++ HotelWorldClocksTest/HotelWorldClocksTest.cs
@@ -40,10 +40,18 @@ namespace HotelWorldClocksTest
         public void the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time()
         {
             //Arrange
+            CityClock londonClock = new CityClock(0);
+            CityClock newYorkClock = new CityClock(-5);
 
+            HotelWorldClockSystem hotelWorldClockSystem = new HotelWorldClockSystem();
+            hotelWorldClockSystem.attach(londonClock);
+            hotelWorldClockSystem.attach(newYorkClock);
+
+            PhoneClock phoneClock = new PhoneClock(8);
 
             //Act
-
+            phoneClock.setHotelWorldClockSystem(hotelWorldClockSystem);
+            phoneClock.setTime(9);
 
             //Assert
             Assert.AreEqual(1, londonClock.getTime());
