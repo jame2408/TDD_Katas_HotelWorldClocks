b556dc23aa327a4901c577259a708560c9471f07
jame2408 <jame2408@gmail.com>
2017-02-28 14:24:54 +0800

    Created constructor of CityClock with a UTC offset time as the parameter.

--- HotelWorldClocks/CityClock.cs
+++ HotelWorldClocks/CityClock.cs
@@ -2,5 +2,11 @@
 {
     public class CityClock
     {
+        private int _utcOffset;
+
+        public CityClock(int utcOffset)
+        {
+            this._utcOffset = utcOffset;
+        }
     }
 }
